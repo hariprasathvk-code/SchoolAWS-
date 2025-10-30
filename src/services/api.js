ee65315873c122f1524fc8cb24182d542eafc22f
hariprasathvk-code <hariprasath.krishnamoorthi@psiog.com>
2025-10-30 16:41:53 +0530

    api env commit

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://cehlyjgiok.execute-api.us-east-2.amazonaws.com/ep37-01';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const api = axios.create({
   baseURL: API_BASE_URL,
