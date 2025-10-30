cb35b22735ca35e8ac55828f8807740ab8194e6d
hariprasathvk-code <hariprasath.krishnamoorthi@psiog.com>
2025-10-30 14:22:04 +0530

    service commit

---
+++ src/services/api.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+
+const API_BASE_URL = 'https://cehlyjgiok.execute-api.us-east-2.amazonaws.com/ep37-01';
+
+const api = axios.create({
+  baseURL: API_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+// Add token to requests
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('idToken');
+  if (token) {
+    config.headers.Authorization = token;
+  }
+  return config;
+});
+
+export const authAPI = {
+  register: (data) => api.post('/register', data),
+  login: (data) => api.post('/login', data),
+};
+
+export const userAPI = {
+  getAllUsers: () => api.get('/users'),
+  createUser: (data) => api.post('/users', data),
+  updateUser: (userId, data) => api.put(`/users/${userId}`, data),
+  deleteUser: (userId) => api.delete(`/users/${userId}`),
+};
+
+export default api;
